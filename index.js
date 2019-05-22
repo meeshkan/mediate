9261b6f1ef1f5ac0c0d7d378186b10f35f4f43fd
pinkbunny1 <snowytrees182@gmail.com>
2019-05-22 18:59:16 +0300

    CircleCI config

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => res.send({ message: "Hello from my app!" }));
+app.get('/', (req, res) => res.send({ message: 'Hello from my app!' }));
 
 // Important: port must be read from the environment for Heroku
 const port = process.env.PORT || 3000;
