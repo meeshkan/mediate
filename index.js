0593e259f3f0f7d6a0cf20f780d12c269de0cb9e
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-05-22 13:29:14 +0300

    Add Node.js boilerplate.

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send({ message: "Hello from my app!" }));
+
+// Important: port must be read from the environment for Heroku
+const port = process.env.PORT || 3000;
+
+console.log(`Listening at port ${port}`);
+
+app.listen(port);
