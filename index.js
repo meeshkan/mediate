eb1c4d92d1e27a55659e6239d7059acf1a653d9d
Hoang <hoang_lp_97@yahoo.com>
2019-05-22 20:05:50 +0300

    test first apit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
-
+const axios = require('axios');
 const app = express();
 
-app.get("/", (req, res) => res.send({ message: "Hello from my app!" }));
+app.get("/", (req, res) => {
+    axios.get('https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=avenger&api-key=KcivkecbgfUqo4J7DgHNdMtw5XgZO0Ey')
+    .then(function (response) {
+        // handle success
+        console.log(typeof response.data.results)
+        res.send(JSON.stringify(response.data.results))
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+});
 
 // Important: port must be read from the environment for Heroku
 const port = process.env.PORT || 3000;
