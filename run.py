1466b6761645c664179a5019c5583ec4c96abd2f
Eva Koppali <lilith.oz812@gmail.com>
2019-05-22 19:55:52 +0300

    Add unmock spotify artist example

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-print("Hello world")
+import requests
+import unmock
+
+def fetchSpotifyArtist():
+    opts = unmock.init()
+    # opts.save = True
+    response = requests.get("https://api.spotify.com/v1/artists")  # will be mocked
+    unmock.reset()
+    return response
+
+
+print(f"respose: {fetchSpotifyArtist()}")
