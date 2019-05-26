1d66091a06002f3798b6104160e24ffe3d65288a
pinkbunny1 <snowytrees182@gmail.com>
2019-05-26 17:40:30 +0300

    Testing

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const express = require("express");
+const express = require('express');
 const axios = require('axios');
 const app = express();
 
-app.get("/", (req, res) => {
-    axios.get('https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=avenger&api-key=KcivkecbgfUqo4J7DgHNdMtw5XgZO0Ey')
-    .then(function (response) {
-        // handle success
-        console.log(typeof response.data.results)
-        res.send(JSON.stringify(response.data.results))
+app.get('/', (req, res) => {
+  axios
+    .get(
+      'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=avenger&api-key=KcivkecbgfUqo4J7DgHNdMtw5XgZO0Ey',
+    )
+    .then(function(response) {
+      // handle success
+
+      // returning json obj to the client
+      res.json(response.data.results);
     })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    });
 });
 
 // Important: port must be read from the environment for Heroku
