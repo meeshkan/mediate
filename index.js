1938625c19284e6c6980fd380a2b6e9ab4acfbcc
KagaK2 <KhanhPhan2512@gmail.com>
2019-05-28 22:59:38 +0300

    Added some tests

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
 const express = require("express");
 const axios = require("axios");
-require("dotenv").config(); //this one for saving the env variables
-
+const onLoad = require("./scripts/onLoad");
 const app = express();
 
-//Please create an application on Spotify Developer's dashboard to get the client id and secret id
-//then either create a .env file in the root of this application when run locally or add those to
-//the config vars on heroku
-const client_id = process.env.SPOTIFY_CLIENT_ID;
-const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-
 let token = "";
-
-const onLoad = async () => {
-  //This function runs on server load, fetching the token through "client credentials" authorization flow from spotify
-  let response = await axios({
-    method: "post",
-    url: "https://accounts.spotify.com/api/token",
-    params: {
-      grant_type: "client_credentials"
-    },
-    headers: {
-      Authorization:
-        "Basic " +
-        Buffer.from(client_id + ":" + client_secret).toString("base64")
-    },
-    json: true
-  });
-
-  let token = response.data.access_token;
-  return token;
-};
 
 (async () => {
   token = await onLoad();
