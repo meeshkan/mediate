55eda4abc36dcc077027aed1405ff56870fd04ec
KagaK2 <KhanhPhan2512@gmail.com>
2019-05-28 22:07:34 +0300

    Revert "Separate onLoad function for testing"
    This reverts commit c779f8ffa6d5b6c4dbc3e4b88402d88d9eb00800.

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const axios = require("axios");
-const onLoad = require("./helpers/onLoad");
+require("dotenv").config(); //this one for saving the env variables
 
 const app = express();
 
-let token = onLoad();
+//Please create an application on Spotify Developer's dashboard to get the client id and secret id
+//then either create a .env file in the root of this application when run locally or add those to
+//the config vars on heroku
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 
-//Useless endpoint
+let token = "";
+
+const onLoad = () =>
+  //This function runs on server load, fetching the token through "client credentials" authorization flow from spotify
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    params: {
+      grant_type: "client_credentials"
+    },
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(client_id + ":" + client_secret).toString("base64")
+    },
+    json: true
+  })
+    .then(response => {
+      token = response.data.access_token;
+    })
+    .catch(err => console.log(err));
+
+onLoad();
+
 app.get("/", (req, res) => res.send({ message: "The server is ready" }));
 
 //End point for searching
